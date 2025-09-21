5ab334935d261a0924443f7502b297c8d6a801f0
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-09-21 15:13:25 +0530

    updated

--- C and C++/Sorting/QuickSort.c
+++ C and C++/Sorting/QuickSort.c
@@ -1,41 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void swap(int *x,int *y){
+void swap(int *x, int *y){
     int temp=*x;
     *x=*y;
     *y=temp;
 }
 
-int partition(int arr[],int l,int h){
-    int pivot=arr[l];
-    int i=l,j=h;
-    do{
-        do{i++;}while(arr[i]<=pivot);
-        do{j--;}while(arr[j]>pivot);
-        if(i<j) swap(&arr[i],&arr[j]);
-    }while(i<j);
-    swap(&arr[l],&arr[j]);
-    return j;
+int Partition(int arr[], int l, int h) {
+    int pivot = arr[l];
+    int i = l - 1;
+    int j = h + 1;
+
+    while (1) {
+        do{i++;} while (arr[i] < pivot);
+        do{j--;} while (arr[j] > pivot);
+        if (i >= j) return j;
+        swap(&arr[i], &arr[j]);
+    }
 }
 
-void QuickSort(int arr[],int l,int h){
+void QuickSort(int A[], int l, int h){
     int j;
     if(l<h){
-        j=partition(arr,l,h);
-        QuickSort(arr,l,j);
-        QuickSort(arr,j+1,h);
+        j=Partition(A,l,h);
+        QuickSort(A,l,j);
+        QuickSort(A,j+1,h);
     }
 }
 
 int main(){
-    int A[]={11,13,7,12,16,9,24,5,10,3},i;
-
-    QuickSort(A,0,10);
-
-    for(i=0;i<10;i++)
+    int A[]={1,4,2,5,7,3,9,8,7,10};
+    int size=sizeof(A)/sizeof(A[0]);
+    QuickSort(A,0,size);
+    for(int i=0;i<size;i++){
         printf("%d ",A[i]);
-    printf("\n");
-
-    return 0;
+    }
 }
