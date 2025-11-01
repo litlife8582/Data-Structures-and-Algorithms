e5838f623fe429b8d75dd04979a8c69f2ef8a9b5
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-11-01 23:27:06 +0530

    tree building

--- C and C++/Trees/Tree.c
+++ C and C++/Trees/Tree.c
@@ -1,4 +1,62 @@
-//
-// Created by Mayuk on 01-11-2025.
-//
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+    struct Node* lChild;
+    struct Node* rChild;
+    int data;
+};
+
+struct Queue{
+    int size;
+    int front;
+    int rear;
+    struct Node **Q;
+};
+
+void create(struct Queue *q,int size){
+    q->size=size;
+    q->front=q->rear=-1;
+    q->Q=(struct Node**)malloc(q->size*sizeof(struct Node*));
+}
+
+struct Node* enqueue(struct Queue *q,struct Node* x){
+    if(q->rear==q->size-1)
+        printf("Queue is full");
+    else{
+        q->rear++;
+        q->Q[q->rear]=x;
+    }
+}
+
+struct Node* dequeue(struct Queue *q){
+    struct Node *x=NULL;
+    if(q->front==q->rear){
+        printf("Queue is Empty");
+    }else{
+        q->front++;
+        x=q->Q[q->front];
+    }
+    return x;
+}
+
+int isEmpty(struct Queue q){
+    return q->front==q->rear;
+}
+
+struct Node* root=NULL;
+int main() {
+    struct Queue q;
+    struct Node *p,*t;
+    root=(struct Node*)malloc(sizeof(struct Node));
+    int x;
+    printf("Enter the root data:");
+    scanf("%d",&x);
+    root->data=x;
+    root->lchild=root->rchild=NULL;
+    enqueue(root);
+
+
+    return 0;
+}
 
