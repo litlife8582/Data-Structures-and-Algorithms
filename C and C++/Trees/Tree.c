650617e1955d1170d73d3e1b324a3c97e56ca797
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-11-02 00:12:46 +0530

    Binary tree

--- C and C++/Trees/Tree.c
+++ C and C++/Trees/Tree.c
@@ -20,7 +20,7 @@ void create(struct Queue *q,int size){
     q->Q=(struct Node**)malloc(q->size*sizeof(struct Node*));
 }
 
-struct Node* enqueue(struct Queue *q,struct Node* x){
+void enqueue(struct Queue *q,struct Node* x){
     if(q->rear==q->size-1)
         printf("Queue is full");
     else{
@@ -40,23 +40,56 @@ struct Node* dequeue(struct Queue *q){
     return x;
 }
 
-int isEmpty(struct Queue q){
+int isEmpty(struct Queue *q){
     return q->front==q->rear;
 }
 
-struct Node* root=NULL;
-int main() {
+struct Node *root=NULL;
+void createTree() {
     struct Queue q;
+    create(&q,100);
     struct Node *p,*t;
     root=(struct Node*)malloc(sizeof(struct Node));
     int x;
     printf("Enter the root data:");
     scanf("%d",&x);
     root->data=x;
-    root->lchild=root->rchild=NULL;
-    enqueue(root);
+    root->lChild=root->rChild=NULL;
+    enqueue(&q,root);
+    while (!isEmpty(&q)){
+        p=dequeue(&q);
+        printf("Enter the left child:");
+        scanf("%d",&x);
+        if (x!=-1) {
+            t=(struct Node*)malloc(sizeof(struct Node));
+            t->data=x;
+            t->lChild=t->rChild=NULL;
+            p->lChild=t;
+            enqueue(&q,t);
+        }
+        printf("Enter the right child:");
+        scanf("%d",&x);
+        if (x!=-1) {
+            t=(struct Node*)malloc(sizeof(struct Node));
+            t->data=x;
+            t->lChild=t->rChild=NULL;
+            p->rChild=t;
+            enqueue(&q,t);
+        }
+    }
+}
 
+void preorder(struct Node *t) {
+    if (t!=NULL) {
+        printf("%d ",t->data);
+        preorder(t->lChild);
+        preorder(t->rChild);
+    }
+}
 
+int main() {
+    createTree();
+    preorder(root);
     return 0;
 }
 
